e6211658d3a80e0ce1ba2c15c912c554f850a7d3
toki513 <tokitahmidkhan@gmail.com>
2024-11-12 13:33:12 +0600

    updated

---
+++ scripts/triangle-calculate.js
@@ -0,0 +1,23 @@
+function calculateTriangleArea(){
+    const triangleBaseInput = document.getElementById('triangle-base')
+
+    const triangleBaseText = triangleBaseInput.value 
+    const base = parseFloat(triangleBaseText)
+
+   
+
+    const triangleHeightInput = document.getElementById('triangle-height')
+
+    const triangleHeightText = triangleHeightInput.value
+    const height = parseFloat(triangleHeightText)
+
+    
+
+    const area = 0.5 * base * height;
+    console.log(area)
+
+    const AreaSpan = document.getElementById('showArea')
+    AreaSpan.innerText = area;
+
+}
+
